0f09caacdf47621f120625f5ae3e710d6ea50477
yinhai <1239128278@qq.com>
2018-05-15 16:45:34 +0800

    第一次创建代码
    第一次创建

---
+++ ordinarycode/src/main/java/com/giveu/ordinary/ordinarycode/OrdinarycodeApplication.java
@@ -0,0 +1,12 @@
+package com.giveu.ordinary.ordinarycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdinarycodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdinarycodeApplication.class, args);
+	}
+}
